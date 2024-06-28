c9f219a7645baf35265894d7806186a57daab17c
Reza IF <rezaif79.ri@gmail.com>
2024-06-28 18:43:54 +0700

    fix: elasticsearch host address on config

--- config/elastic_config.go
+++ config/elastic_config.go
@@ -4,7 +4,7 @@ import "github.com/elastic/go-elasticsearch/v8"
 
 func InitElasticClient() (*elasticsearch.Client, error) {
 	return elasticsearch.NewClient(elasticsearch.Config{
-		Addresses: []string{"localhost:9200"},
+		Addresses: []string{"http://localhost:9200"},
 		Username:  "",
 		Password:  "",
 	})
