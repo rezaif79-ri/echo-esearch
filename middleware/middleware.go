414d1ecc59a3928b521393fa91eeb637e090be21
Reza IF <rezaif79.ri@gmail.com>
2024-06-28 17:19:00 +0700

    feat: implements request logger with zap on middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,45 +1,47 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"go.uber.org/zap"
 )
 
 func SetEchoMiddleware(e *echo.Echo) {
-	// e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-	// 	LogStatus: true,
-	// 	LogURI:    true,
-	// 	BeforeNextFunc: func(c echo.Context) {
-	// 		c.Set("customValueFromContext", 42)
-	// 	},
-	// 	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-	// 		value, _ := c.Get("customValueFromContext").(int)
-	// 		fmt.Printf("REQUEST: uri: %v, status: %v, custom-value: %v\n", v.URI, v.Status, value)
-	// 		return nil
-	// 	},
-	// }))
-
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogLatency:       true,
-		LogProtocol:      true,
-		LogRemoteIP:      true,
-		LogHost:          true,
-		LogMethod:        true,
-		LogURI:           true,
-		LogURIPath:       true,
-		LogRoutePath:     true,
-		LogRequestID:     true,
-		LogReferer:       true,
-		LogUserAgent:     true,
-		LogStatus:        true,
-		LogError:         true,
-		LogContentLength: true,
-		LogResponseSize:  true,
+		LogURI:    true,
+		LogStatus: true,
 		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			fmt.Printf("%s %s %dms, Size: %d\n", v.Method, v.URIPath, v.Latency.Milliseconds(), v.ResponseSize)
+
+			logger.Info("request",
+				zap.String("URI", v.URI),
+				zap.Int("status", v.Status),
+			)
+
 			return nil
 		},
 	}))
+
+	// e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+	// 	LogLatency:       true,
+	// 	LogProtocol:      true,
+	// 	LogRemoteIP:      true,
+	// 	LogHost:          true,
+	// 	LogMethod:        true,
+	// 	LogURI:           true,
+	// 	LogURIPath:       true,
+	// 	LogRoutePath:     true,
+	// 	LogRequestID:     true,
+	// 	LogReferer:       true,
+	// 	LogUserAgent:     true,
+	// 	LogStatus:        true,
+	// 	LogError:         true,
+	// 	LogContentLength: true,
+	// 	LogResponseSize:  true,
+	// 	LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+	// 		fmt.Printf("%s %s %dms, Size: %d\n", v.Method, v.URIPath, v.Latency.Milliseconds(), v.ResponseSize)
+	// 		return nil
+	// 	},
+	// }))
 }
