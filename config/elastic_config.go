af4d51e3f4c6828e48045d17c7fed317298d6d1a
Reza IF <rezaif79.ri@gmail.com>
2024-07-04 10:15:21 +0700

    feat: add transport client var on elastic config to handle connection timeout or refused error

--- config/elastic_config.go
+++ config/elastic_config.go
@@ -1,11 +1,29 @@
 package config
 
-import "github.com/elastic/go-elasticsearch/v8"
+import (
+	"crypto/tls"
+	"net"
+	"net/http"
+	"time"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
 
 func InitElasticClient() (*elasticsearch.Client, error) {
+	client := http.Transport{
+		MaxIdleConnsPerHost:   10,
+		ResponseHeaderTimeout: time.Second * 2,
+		DialContext:           (&net.Dialer{Timeout: time.Second * 2}).DialContext,
+		TLSClientConfig: &tls.Config{
+			MaxVersion:         tls.VersionTLS11,
+			InsecureSkipVerify: true,
+		}}
+
 	return elasticsearch.NewClient(elasticsearch.Config{
+
 		Addresses: []string{"http://localhost:9200"},
 		Username:  "",
 		Password:  "",
+		Transport: &client,
 	})
 }
